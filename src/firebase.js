063f08725a496b7fbf5e4e039f07db72bd91047a
Omkar Reddy <58789899+AcerolaOrion420@users.noreply.github.com>
2024-02-02 04:58:36 +0530

    added api key

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDtoGmJ6xs7GVgexWIdRkiBt47l3t5wuCQ",
+  authDomain: "testproj-48f04.firebaseapp.com",
+  projectId: "testproj-48f04",
+  storageBucket: "testproj-48f04.appspot.com",
+  messagingSenderId: "600681312047",
+  appId: "1:600681312047:web:b8361cb222490161c1bbda",
+  measurementId: "G-GX6ZSN4JH4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
